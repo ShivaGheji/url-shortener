db6c17dee4de15d954ff9a594a155c7115974544
Shivanand Gheji <shivagheji@gmail.com>
2025-08-24 22:01:49 +0530

    refactor(validatior): replace joi with zod for schema validation

--- backend/src/api/v1/validators/auth.validator.js
+++ backend/src/api/v1/validators/auth.validator.js
@@ -1,12 +1,31 @@
-import Joi from "joi";
+import { z } from "zod";
 
-export const registerSchema = Joi.object({
-  name: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
+const nameValidation = z
+  .string()
+  .min(4, "Name must be at least 4 characters")
+  .max(30, "Name must be at most 60 characters");
+
+const passwordValidation = z
+  .string()
+  .regex(
+    /^(?=.*[A-Z])(?=.*[!@#$%^&*()_\-+={}[\]|;:'",.<>/?]).{8,16}$/,
+    "Password must be 8-16 chars, include 1 uppercase & 1 special char"
+  );
+
+const emailValidation = z.string().email("Invalid email format");
+
+export const registerSchema = z.object({
+  name: nameValidation,
+  email: emailValidation,
+  password: passwordValidation,
 });
 
-export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+export const loginSchema = z.object({
+  email: emailValidation,
+  password: passwordValidation,
+});
+
+export const updatePasswordSchema = z.object({
+  oldPassword: z.string().min(1, "Current password is required"),
+  newPassword: passwordValidation,
 });
