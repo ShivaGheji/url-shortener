80475b8e89332d2b7744eb88f927106d8d283f5b
Shivanand Gheji <shivagheji@gmail.com>
2025-08-24 22:07:06 +0530

    refactor(rateLimitMiddleware): restructured ratelimiter

--- backend/src/api/v1/middlewares/rateLimiter.middleware.js
+++ backend/src/api/v1/middlewares/rateLimiter.middleware.js
@@ -1,46 +1,51 @@
 import urlModel from "../models/url.model.js";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "../../../config/env.js";
+import { JWT_SECRET, COOKIE_NAME } from "../../../config/env.js";
 import User from "../models/user.model.js";
 import getClientMeta from "../../../utils/getClientMeta.js";
 
 export const limitAnonymousUrls = async (req, res, next) => {
-  let token;
+  try {
+    const token =
+      req.cookies[COOKIE_NAME] || req.headers.authorization?.split(" ")[1];
 
-  //   console.log("checking headers for token");
-  //   console.log(req.headers.authorization);
+    if (token) {
+      try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        const user = await User.findById(decoded.userId).select("-password");
 
-  const authHeader = req.headers.authorization;
+        if (user && user.tokenVersion == decoded.tv) {
+          req.user = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            tokenVersion: user.tokenVersion,
+          };
+          return next();
+        }
+      } catch (err) {
+        next(err);
+      }
+    }
 
-  //   if (
-  //     req.headers.authorization &&
-  //     req.headers.authorization.startswith("Bearer ")
-  if (authHeader && authHeader.startsWith("Bearer ")) {
-    token = req.headers.authorization.split(" ")[1];
+    // Anonymous logic
+    const { ip } = getClientMeta(req);
+
+    const count = await urlModel.countDocuments({
+      createdBy: null,
+      "clientMeta.ip": ip,
+    });
+
+    if (count >= 3) {
+      return res.status(403).json({
+        success: false,
+        message: "Limit reached. Please log in to create more URLs.",
+      });
+    }
+
+    req.clientMeta = { ip };
+    next();
+  } catch (err) {
+    next(err);
   }
-
-  const decoded = jwt.verify(token, JWT_SECRET);
-
-  const user = await User.findById(decoded.userID);
-
-  if (user) {
-    req.user = user;
-    return next(); // Authenticated user = skip
-  }
-  //   const { ip, userAgent } = getClientMeta(req);
-  const { ip } = getClientMeta(req);
-
-  const count = await urlModel.countDocuments({
-    createdBy: null,
-    clientMeta: { ip },
-  });
-
-  if (count >= 3) {
-    return res
-      .status(403)
-      .json({ message: "Limit reached. Please log in to create more URLs." });
-  }
-
-  req.clientMeta = { ip }; // Pass to controller
-  next();
 };
